3c1a4b02811a5c3c340687f4b16c4166a8e4f3f4
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2023-12-02 23:48:38 -0300

    feat: database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,37 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: false,
+      migrationsTableName: "custom_migration_table",
+      entities: [
+        __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+      migrations: [
+        '../migration/'
+      ],
+      subscribers: [],
+
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
