20d3ecaeef1f834b99519b804c3f0b7f57c1ae07
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2023-12-06 16:52:59 -0300

    feat: jwt config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,25 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([
       User
-    ])
+    ]),
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: '8h'
+        }
+      })
+    })
   ],
   controllers: [UsersController],
   providers: [UsersService],
